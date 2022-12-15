c8fd31cf575e6c4debc61e4ebdfc166d40b4128c
Dzikrul <dzikrul1616@student.ub.ac.id>
2022-12-15 23:37:41 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
 
 void main() async {
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "ROLLS",
+      title: "SI Pergudangan",
       initialRoute: Routes.BARANG_MASUK,
       getPages: AppPages.routes,
     );
