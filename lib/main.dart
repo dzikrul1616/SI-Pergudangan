c26538da9e4182773f426387565bed856e237101
M. Dzikrull Hakim <91420806+dzikrul1616@users.noreply.github.com>
2022-12-16 09:27:43 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "SI Pergudangan",
-      initialRoute: Routes.BARANG_MASUK,
+      initialRoute: Routes.LOGIN,
       getPages: AppPages.routes,
     );
   }
